5a1bedb4cfec627a84205efa7c70ba2b6741d2c9
Nishanth019 <bhukyanishanth69@gmail.com>
2024-03-04 22:19:29 +0530

    signIn and signUp auth setup

---
+++ src/models/College.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema; 
+
+const collegeSchema = new Schema({
+    college_name: {
+        type: String,
+        required: true
+    },
+    street_name: {
+        type: String
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    pincode: {
+        type: Number,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    email_domain: {
+        type: String,
+        required: true
+    }
+});
+
+exports.User = mongoose.model("College", collegeSchema);
