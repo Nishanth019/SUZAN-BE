5653d11e566328401429a4f7f192698469aa9f9b
Manish Chandolu <manishchandolu@gmail.com>
2024-08-07 01:15:25 +0530

    added functionality to give admin access

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -15,6 +15,70 @@ class UserController {
       user,
     });
   };
+  getUserByEmail = async (req, res) => {
+    const { email } = req.query;
+    // console.log(100,email)
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  };
+
+  // Update user by ID
+  updateUser = async (req, res) => {
+    try {
+      const { id } = req.user;
+      const updatedData = req.body;
+      const updatedUser = await User.findByIdAndUpdate(id, updatedData, {
+        new: true,
+      });
+      console.log(1, updatedUser);
+      if (!updatedUser) {
+        return res
+          .status(404)
+          .json({ success: false, message: "User not found" });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "User Details updated successfully",
+        user: updatedUser,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: "Internal Server Error" });
+    }
+  };
+  updateUserRole = async (req, res) => {
+    try {
+      const { id } = req.body;
+      console.log(2, id);
+      const updatedData = req.body;
+      const updatedUser = await User.findByIdAndUpdate(id, updatedData, {
+        new: true,
+      });
+      if (!updatedUser) {
+        return res
+          .status(404)
+          .json({ success: false, message: "User not found" });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "User Details updated successfully",
+        user: updatedUser,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: "Internal Server Error" });
+    }
+  };
 
   deleteUser = async (req, res) => {
     try {
@@ -30,33 +94,6 @@ class UserController {
       res
         .status(200)
         .json({ success: true, message: "User deleted successfully" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ success: false, error: "Internal Server Error" });
-    }
-  };
-
-  // Update user by ID
-  updateUser = async (req, res) => {
-    try {
-      const { id } = req.user;
-      const updatedData = req.body;
-
-      const updatedUser = await User.findByIdAndUpdate(id, updatedData, {
-        new: true,
-      });
-
-      if (!updatedUser) {
-        return res
-          .status(404)
-          .json({ success: false, message: "User not found" });
-      }
-
-      res.status(200).json({
-        success: true,
-        message: "User Details updated successfully",
-        user: updatedUser,
-      });
     } catch (error) {
       console.error(error);
       res.status(500).json({ success: false, error: "Internal Server Error" });
@@ -132,26 +169,37 @@ class UserController {
     }
   };
 
-    // Upload user picture
-    uploadPicture = async (req, res) => {
-      try {
-        console.log(1, req.file, req.user.id);
-        const { id } = req.user;
-        const imageUrl = req.file.location; // Assuming Multer-S3 provides 'location' for the uploaded file
+  // Upload user picture
+  uploadPicture = async (req, res) => {
+    try {
+      console.log(1, req.file, req.user.id);
+      const { id } = req.user;
+      const imageUrl = req.file.location; // Assuming Multer-S3 provides 'location' for the uploaded file
 
-        
-        const updatedUser = await User.findByIdAndUpdate(id, { picture: imageUrl }, { new: true });
-  
-        if (!updatedUser) {
-          return res.status(404).json({ success: false, message: 'User not found' });
-        }
-  
-        res.status(200).json({ success: true, message: 'User picture uploaded successfully', user: updatedUser });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, error: 'Internal Server Error' });
+      const updatedUser = await User.findByIdAndUpdate(
+        id,
+        { picture: imageUrl },
+        { new: true }
+      );
+
+      if (!updatedUser) {
+        return res
+          .status(404)
+          .json({ success: false, message: "User not found" });
       }
-    };
+
+      res
+        .status(200)
+        .json({
+          success: true,
+          message: "User picture uploaded successfully",
+          user: updatedUser,
+        });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: "Internal Server Error" });
+    }
+  };
 }
 
 module.exports.UserController = new UserController();
