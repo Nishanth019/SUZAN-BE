4c405507792f9accdc38303e9d1c531ec1553207
Nishanth <122528380+Nishanth019@users.noreply.github.com>
2024-10-04 21:47:58 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,23 @@
-require("dotenv").config(); // intitializing env file
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const http = require('http');
 
-require("./startup/index.startup")(app); // strating server
+require("./startup/index.startup")(app);
+
+const keepServerAlive = () => {
+  setInterval(() => {
+    http.get(process.env.SERVER_URL, (res) => {
+      console.log(`Pinged server with status: ${res.statusCode}`);
+    }).on('error', (err) => {
+      console.error(`Failed to ping server: ${err.message}`);
+    });
+  }, 5 * 60 * 1000);
+};
+
+keepServerAlive();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
