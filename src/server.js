f243aab14eb3f2c246a004c5b7d9b2439cdc6178
Nishanth <122528380+Nishanth019@users.noreply.github.com>
2024-10-04 22:10:02 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,33 +1,5 @@
-require("dotenv").config();
+require("dotenv").config(); // intitializing env file
 const express = require("express");
 const app = express();
-const http = require('http');
 
-require("./startup/index.startup")(app);
-
-const keepServerAlive = () => {
-  setInterval(() => {
-    http.get(process.env.SERVER_URL, (res) => {
-      console.log(`Pinged server with status: ${res.statusCode}`);
-    }).on('error', (err) => {
-      console.error(`Failed to ping server: ${err.message}`);
-    });
-  }, 5 * 60 * 1000);
-};
-
-const startServer = (port) => {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  }).on('error', (err) => {
-    if (err.code === 'EADDRINUSE') {
-      console.log(`Port ${port} is in use. Trying port ${port + 1}...`);
-      startServer(port + 1); // Try the next port
-    } else {
-      console.error('Error starting server:', err);
-    }
-  });
-};
-
-const PORT = process.env.PORT || 8080; // Start from the defined port
-keepServerAlive();
-startServer(PORT);
+require("./startup/index.startup")(app); // strating server
