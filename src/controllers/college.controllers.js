0d690d4a2fc6b9952e4c1bed171beb0c64b001a1
Manish Chandolu <manishchandolu@gmail.com>
2024-11-13 23:56:33 +0530

    added college logo

--- src/controllers/college.controllers.js
+++ src/controllers/college.controllers.js
@@ -2,19 +2,18 @@ const { User } = require("../models/User.model.js");
 const { College } = require("../models/College.model.js");
 
 class CollegeController {
-
-    // Get all verified colleges
-    getAllVerifiedColleges = async (req, res) => {
-      try {
-        console.log(1);
-        const colleges = await College.find({ isVerified: true });
-        console.log(2);
-        res.status(200).json({ colleges: colleges, success: true });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error", success: false });
-      }
-    };
+  // Get all verified colleges
+  getAllVerifiedColleges = async (req, res) => {
+    try {
+      console.log(1);
+      const colleges = await College.find({ isVerified: true });
+      console.log(2);
+      res.status(200).json({ colleges: colleges, success: true });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error", success: false });
+    }
+  };
 
   // Get all colleges
   getAllColleges = async (req, res) => {
@@ -81,12 +80,88 @@ class CollegeController {
           .json({ error: "College not found", success: false });
       }
 
-      res.status(200).json({ college: updatedCollege, success: true, message: "College Details updated successfully" });
+      res.status(200).json({
+        college: updatedCollege,
+        success: true,
+        message: "College Details updated successfully",
+      });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Internal Server Error", success: false });
     }
   };
-}
 
+  // uploadLogo = async (req, res) => {
+  //   try {
+  //     // console.log(1, req.file,);
+  //     const { id } = req.user;
+  //     const imageUrl = req.file.location; // Assuming Multer-S3 provides 'location' for the uploaded file
+
+  //     const updatedUser = await User.findByIdAndUpdate(
+  //       id,
+  //       { picture: imageUrl },
+  //       { new: true }
+  //     );
+
+  //     if (!updatedUser) {
+  //       return res
+  //         .status(404)
+  //         .json({ success: false, message: "User not found" });
+  //     }
+
+  //     res.status(200).json({
+  //       success: true,
+  //       message: "User picture uploaded successfully",
+  //       user: updatedUser,
+  //     });
+  //   } catch (error) {
+  //     console.error(error);
+  //     res.status(500).json({ success: false, error: "Internal Server Error" });
+  //   }
+  // };
+
+  uploadLogo = async (req, res) => {
+    try {
+      const pictureUrl = req.file.location; // Assuming Multer-S3 provides 'location' for the uploaded file
+      console.log(pictureUrl);
+      res.status(200).json({
+        success: true,
+        message: "Logo added successfully",
+        picture: pictureUrl,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: "Internal Server Error" });
+    }
+  };
+
+  updateLogo = async (req, res) => {
+    try {
+      // console.log(1, req.file, req.user.id);
+      const { id } = req.params;
+      console.log(22,id)
+      const logoUrl = req.file.location; 
+      const updatedCollege = await College.findByIdAndUpdate(
+        id,
+        { college_logo: logoUrl },
+        { new: true }
+      );
+      
+      if (!updatedCollege) {
+        return res
+          .status(404)
+          .json({ success: false, message: "College not found" });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "College logo updated successfully",
+        college: updatedCollege,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: "Internal Server Error" });
+    }
+  };
+}
 module.exports.CollegeController = new CollegeController();
--- src/routes/college.routes.js
+++ src/routes/college.routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { CollegeController } = require("../controllers/college.controllers.js");
 const { Auth } = require("../middlewares/auth.middlewares.js");
-
+const upload = require("../middlewares/file-upload.middlewares.js");
 // Route for accesing the college form id
 router.get('/getcollegefromid/:id',CollegeController.getCollegeFromId);
-
+ 
 // Route for accesing all the colleges
 router.get('/',CollegeController.getAllColleges); 
 
@@ -18,4 +18,17 @@ router.delete("/:id", CollegeController.deleteCollege);
 // Route for accesing all the colleges
 router.put("/:id", CollegeController.updateCollege);
 
+
+
+router.post(
+  "/uploadlogo",
+  upload.single("picture"),
+  CollegeController.uploadLogo
+);
+
+router.post(
+  "/updatelogo/:id",
+  upload.single("picture"),
+  CollegeController.updateLogo
+);
 module.exports.CollegeRouter = router;
